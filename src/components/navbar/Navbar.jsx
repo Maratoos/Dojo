8da65a2b31b9a2f0c3cea86baf6e25eed5b31dd7
Maratoos <kudushev01@mail.ru>
2022-06-29 23:28:31 +0600

    CreatedLogoutFcAndSomeFixesInSignupFcAndNavbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import Temple from '../../assets/temple.svg'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../../hooks/useLogout'
 import './styles.css'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 const Navbar = () => {
+  const { logout, isPending, error } = useLogout()
+  const { user } = useAuthContext()
   return (
     <nav className="navbar">
         <ul>
             <li className='logo'>
                 <img src={Temple} alt="logo" />
                 <span>The Dojo</span>
-                </li>
+            </li>
+            {!user && <>          
             <li>
                 <Link to="/login">Login</Link>
-                </li>
+            </li>
             <li>
                 <Link to="/signup">Signup</Link>
-                </li>
+            </li>
+            </>}
+            {user &&    
             <li>
-                <button className='btn'> Logout</button>
-                </li>
+                {!isPending && <button onClick={logout} className="btn">Logout</button>}
+                {isPending && <button className="btn">Loading...</button>}
+                {error && <div className='error'>{error}</div>}
+            </li>
+            }
         </ul>
     </nav>
   )
