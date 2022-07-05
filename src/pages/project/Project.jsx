b0563f894d6239f182853e278e29757df6c5a827
Maratoos <kudushev01@mail.ru>
2022-07-05 21:43:43 +0600

    ProjectPageFix

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useDocument } from '../../hooks/useDocument'
+import { useParams } from 'react-router-dom'
 import './styles.css'
+import ProjectSummary from './ProjectSummary'
 
 const Project = () => {
+  const params = useParams()
+  const { document, error } = useDocument("projects", params.id)
+
+  if(error) {
+    return <div className='error'>{error}</div>
+  }
+  if(!document) {
+    return <div className='loading'>Loading...</div>
+  }
+
   return (
-    <div>Project</div>
+    <div className='project-details'>
+      <ProjectSummary project={document} />
+    </div>
   )
 }
 
